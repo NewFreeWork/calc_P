1c73c41725fad79c08b05618a6e322a39030c54a
kindbiny2 <63532169+kindbiny2@users.noreply.github.com>
2020-04-15 11:41:23 +0900

    xamarin forms commit
    plus xamarin project file all commit

---
+++ forms_plus/forms_plus/forms_plus/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace forms_plus
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+#if false       //kindbiny_20200415 네비게이션 페이지로 변경
+            MainPage = new MainPage();
+#else
+            MainPage = new NavigationPage(new MainPage());
+#endif
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
