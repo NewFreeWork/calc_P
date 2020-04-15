17a23247224c0e50305f1d53664308b4291eaa04
kindbiny2 <63532169+kindbiny2@users.noreply.github.com>
2020-04-15 11:18:03 +0900

    commit test
    test

--- xamarin_form_plus/xamarin_form_plus/xamarin_form_plus.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ xamarin_form_plus/xamarin_form_plus/xamarin_form_plus.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -70,7 +70,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"6c368c28-565b-45ee-8d63-c7fa795023c8"
+	.asciz	"cec98ced-6832-4b98-bc81-4235c75eac62"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
