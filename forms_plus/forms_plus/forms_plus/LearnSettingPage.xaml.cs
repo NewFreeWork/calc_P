7419339f573653b9337f66cbdb42ada60f3fd654
jscho77 <dreamguy01@naver.com>
2020-04-19 00:18:05 +0900

    Learn Page 코드 추가
    Learn Page 코드 추가
    신규 페이지 추가

---
+++ forms_plus/forms_plus/forms_plus/LearnSettingPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace forms_plus
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LearnSettingPage : ContentPage
+    {
+        public LearnSettingPage()
+        {
+            InitializeComponent();
+        }
+      
+        private async void StartLearn_Clicked(object sender, EventArgs e)
+        {
+            bool setUpOnOff = false;
+
+            if (EntryUp.Text == "O")
+            {
+                setUpOnOff = true;
+            }
+            
+            LearnSetSington.Instance.setNdigit = Int32.Parse(EntryNdigit.Text);          
+            LearnSetSington.Instance.setUpOnOff = setUpOnOff;
+            LearnSetSington.Instance.setQ_Num = Int32.Parse(EntryQNum.Text);
+
+            await Navigation.PushAsync(new LearnningPage());
+        }
+    }
+}
---
+++ forms_plus/forms_plus/forms_plus/LearnSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace forms_plus
+{
+    public class LearnSetSington
+    {
+
+        private static LearnSetSington instance = null;
+        public static LearnSetSington Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new LearnSetSington();
+                }
+                return instance;
+            }
+        }
+
+        private LearnSetSington()
+        {
+        }
+
+        public int setNdigit; // 선언할 변수들 
+        public bool setUpOnOff; // 선언할 변수들 
+        public int setQ_Num; // 선언할 변수들 
+    }
+
+}
