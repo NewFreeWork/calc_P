a9f44752cfe389fb8fe9e1970df56ba02696e18a
jscho77 <dreamguy01@naver.com>
2021-02-22 23:37:34 +0900

    1. AdMob 광고 관련 코드 및 패키지 추가

---
+++ forms_plus/forms_plus/forms_plus/Models/CalendarInfo.cs
@@ -0,0 +1,30 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace forms_plus.Models
+{
+    public class CalendarLearnInfo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Idx { get; set; }
+        public String Usr { get; set; }
+        public String LearnDate { get; set; }
+        public bool LearnDone { get; set; }
+        public String LearnStage { get; set; }
+   
+    }
+
+    public class CalendarTestInfo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Idx { get; set; }
+        public String Usr { get; set; }
+        public String TestDate { get; set; }
+        public bool TestDone { get; set; }
+        public String TestStage { get; set; }
+        public int TestScore { get; set; }
+        public String TestTime { get; set; }
+    }
+}
